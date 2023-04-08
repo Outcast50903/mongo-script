292ae2f06c7e8ca925309b90525ec54b15a3d530
Diego Garcia <dgarciavallejos@gmail.com>
2023-04-08 19:55:47 -0400

    add readXLSXFile function

---
+++ utils/readFile/index.ts
@@ -0,0 +1,11 @@
+import { readFile, utils } from 'xlsx';
+
+const readXLSXFile = <T>(path: string): Array<T> => {
+  const workbook = readFile(path);
+  const sheetName = workbook.SheetNames[0];
+  return utils.sheet_to_json(workbook.Sheets[sheetName], {
+    raw: false,
+  });
+}
+
+export default readXLSXFile;
