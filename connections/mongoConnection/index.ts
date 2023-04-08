860e08d4ef21d18386e1b0d9806f419d43b0055e
Diego Garcia <dgarciavallejos@gmail.com>
2023-04-08 19:54:48 -0400

    update connections

--- connections/mongoConnection/index.ts
+++ connections/mongoConnection/index.ts
@@ -1,28 +1,54 @@
-import { MongoClient } from "mongodb";
+import { Collection, Db, MongoClient } from "mongodb";
 import { IMongoDB } from "../../types";
-
+import createLog from "../../utils/createLog";
 export class MongoConnection implements IMongoDB {
   private client: MongoClient | undefined;
+  private db: Db | undefined;
 
-  async connect() {
+  constructor() {
+    this.connect();
+  }
+
+  private async connect(): Promise<void> {
     console.clear();
     console.log("------------------------------------------------------------------------------------------------------");
     console.log("Iniciando conexión de mongo ...");
-    return this.client = await MongoClient.connect(
+    this.client = await MongoClient.connect(
       process.env.MONGO_URL ?? "",
       // @ts-ignore
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
+    this.db = this.client?.db(process.env.MONGO_DB ?? "");
   }
 
-  createCollection(collectionName: string) {
+  createCollection(collectionName: string): Collection<Document> | undefined{
     console.log("Iniciando colección ...");
-    const db = this.client?.db(process.env.MONGO_DB ?? "");
-    return db?.collection(collectionName);
+    return this.db?.collection(collectionName);
   }
 
   close() {
     console.log("Desconectando ...");
     this.client?.close();
   }
+
+    // @ts-ignore
+    async createBulk(
+      collection: Collection<Document>, 
+      bulkArr: any[],
+      collectionAlias: string = 'documentos'
+    ) {
+      try {
+        const result = await collection?.bulkWrite(bulkArr, { ordered: false })
+        console.log(`Se han encontrado ${result.matchedCount} ${collectionAlias}`);
+        console.log(`Se han modificado ${result.modifiedCount} ${collectionAlias}`);
+        console.log(`Se han eliminado ${result.deletedCount} ${collectionAlias}`);
+        console.log(`Se han insertado ${result.insertedCount} ${collectionAlias}`);
+        const notModified = result.insertedCount - result.modifiedCount;
+        notModified !== 0 && console.warn(`No se han modificado ${notModified} ${collectionAlias}`);
+        console.log(result);
+      } catch (error) {
+        this.close();
+        createLog(error)
+      }
+    }
 }
--- connections/typegooseConnection/index.ts
+++ connections/typegooseConnection/index.ts
@@ -1,24 +1,24 @@
 import { ReturnModelType, getModelForClass, mongoose } from "@typegoose/typegoose";
-import Contracts from "../../schemas/contracts";
+import { BeAnObject } from "@typegoose/typegoose/lib/types";
 import { IMongoDB } from "../../types";
+import createLog from "../../utils/createLog";
 
 export class TypegooseConnection implements IMongoDB {
   private client: typeof mongoose | undefined;
 
-  async connect() {
+  constructor() {
+    this.connect();
+  }
+
+  private async connect(): Promise<void> {
     console.clear();
     console.log("---------------------------------------------------------------------------------------------------");
     console.log("Iniciando conexión de mongo ...");
-    return this.client = await mongoose.connect(
+    this.client = await mongoose.connect(
       process.env.MONGO_URL ?? "",
       // @ts-ignore
       { useNewUrlParser: true, useUnifiedTopology: true, dbName: process.env.MONGO_DB ?? "" }
     )
-  }
-
-  createCollection() {
-    console.log("Iniciando colección ...");
-    return getModelForClass(Contracts);
   }
 
   createCollectionTypegoose<T>(cls: new (...args: any[]) => T) {
@@ -29,5 +29,24 @@ export class TypegooseConnection implements IMongoDB {
   close() {
     console.log("Desconectando ...");
     this.client?.disconnect();
+  }
+
+  // @ts-ignore
+  async createBulk<T>(
+    collection: ReturnModelType<new (...args: any[]) => T, BeAnObject>, 
+    bulkArr: any[],
+    collectionAlias: string = 'documentos'
+  ) {
+    try {
+      const result = await collection?.bulkWrite(bulkArr, { ordered: false })
+      console.log(`Se han encontrado ${result.matchedCount} ${collectionAlias}`);
+      console.log(`Se han modificado ${result.modifiedCount} ${collectionAlias}`);
+      console.log(`Se han eliminado ${result.deletedCount} ${collectionAlias}`);
+      console.log(`Se han insertado ${result.insertedCount} ${collectionAlias}`);
+      console.log(result);
+    } catch (error) {
+      this.close();
+      createLog(error)
+    }
   }
 }
