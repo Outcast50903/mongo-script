4495960ca9b0976d2f9d4fa97eb1ffd912549568
Diego Garcia <dgarciavallejos@gmail.com>
2023-04-08 00:09:37 -0400

    add types

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,18 @@
+import { ReturnModelType, mongoose } from "@typegoose/typegoose";
+import { BeAnObject } from "@typegoose/typegoose/lib/types";
+import { MongoClient } from "mongodb";
+import { Collection } from "mongoose";
+
 export type FileType = {
-  Id: string
-  name: string
+  id: string
+  vin: string
+  action: 'CADUCAR' | 'RENOVAR'  | 'SUSPENDER'
+  reactivationDate: string
 };
+
+export interface IMongoDB {
+  connect: () => Promise<MongoClient | typeof mongoose>;
+  createCollection: (collectionName: string) => Collection<mongoose.mongo.BSON.Document> | ReturnModelType<any, BeAnObject> | undefined;
+  close: () => void;
+  createCollectionTypegoose?:<T> (cls: new (...args: any[]) => T) => ReturnModelType<any, BeAnObject> | undefined;
+}
