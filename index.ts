15cd0ac26e1a4f725754982793730515fa1c33c9
Diego Garcia <dgarciavallejos@gmail.com>
2023-04-08 20:02:21 -0400

    update index

--- index.ts
+++ index.ts
@@ -1,103 +1,87 @@
 import * as dayjs from 'dayjs';
 import * as dotenv from 'dotenv';
-import { readFile, utils } from 'xlsx';
 import { mongoose } from "@typegoose/typegoose";
-import { AnyBulkWriteOperation, ObjectId } from 'mongodb';
+import { AnyBulkWriteOperation } from 'mongodb';
+import { TypegooseConnection } from './connections';
+import { Contracts, UpdatedBy, Vehicles } from './schemas';
+import readXLSXFile from './utils/readFile';
+import { FileType } from './types';
+import createLog from './utils/createLog';
+import isValidVin from './utils/vinValidator';
 
-import { FileType } from 'types';
-import createLog from 'utils/createLog';
-import { TypegooseConnection } from 'connections';
-import { Contracts, UpdatedBy, Vehicles } from 'schemas';
 
 const main = async () => {
   try {
     const db = new TypegooseConnection();
-    
-    db.connect().then(async () => {
-      const contractsCollection = db.createCollectionTypegoose(Contracts);
-      const vehiclesCollection = db.createCollectionTypegoose(Vehicles);
+    const contractsCollection = db.createCollectionTypegoose(Contracts);
+    const vehiclesCollection = db.createCollectionTypegoose(Vehicles);
 
-      const contractBulkArr: AnyBulkWriteOperation<Contracts>[] = [];
-      const vehiclesBulkArr: AnyBulkWriteOperation<Vehicles>[] = [];
-      const errId: string[] = [];
+    const contractBulkArr: AnyBulkWriteOperation<Contracts>[] = [];
+    const vehiclesBulkArr: AnyBulkWriteOperation<Vehicles>[] = [];
+    const errId: FileType[] = [];
 
-      const workbook = readFile(process.env.FILE_PATH ?? '');
-      const sheetName = workbook.SheetNames[0];
-      const inputRows: FileType[] = utils.sheet_to_json(workbook.Sheets[sheetName], {
-        raw: false,
-      });
+    const inputRows = readXLSXFile<FileType>(process.env.FILE_PATH ?? '');
+    inputRows.forEach(async row => {        
+      if(!mongoose.isValidObjectId(row.id))
+      {
+        errId.push(row);
+        return;
+      };
 
-      inputRows.forEach(async row => {        
-        const _id = new mongoose.Types.ObjectId(row.id);
-        !mongoose.isValidObjectId(_id) && errId.push(row.id);
-        
-        const reactivationDate = new Date(dayjs(row.reactivationDate).format('YYYY/MM/DD'));
-        const action = row.action === 'CADUCAR' ? 'expire' : row.action === 'RENOVAR' ? 'renewal' : 'suspend';
-        const contractActions = row.action === 'CADUCAR' 
-          ? { active: false, enabled: false, deletedAt: new Date(dayjs().format('YYYY/MM/DD')) }
-            : row.action === 'RENOVAR' 
-              ? { isPremium: true, reactivationDate }
-              : { isPremium: false, reactivationDate };
-        const vehicleActions = row.action === 'CADUCAR' 
-          ? { enabled: false, deletedAt: new Date(dayjs().format('YYYY/MM/DD')) }
-            : row.action === 'RENOVAR' 
-              ? { isContractSuspended: false, isPremium: true } 
-              : { isContractSuspended: true, isPremium: false };
-        const updatedBy: UpdatedBy[] = [
-          {
-            updatedBy: new mongoose.Types.ObjectId(process.env.ADMIN_ID ?? ''),
-            updatedAt: new Date(dayjs().format('YYYY/MM/DD')),
-            action,
-          }
-        ];
-        
-        vehiclesBulkArr.push({
-          updateOne: {
-            filter: { vin: row.vin },
-            //@ts-ignore
-            update: { $set: { ...vehicleActions } }
-          }
-        });
+      if (!isValidVin(row.vin)) {
+        errId.push(row);
+        return;
+      }
 
-        contractBulkArr.push({
-          updateOne: {
-            filter: { _id },
-            update: { $set: { updatedBy, ...contractActions } }
-          }
-        });    
-      });
+      const _id = new mongoose.Types.ObjectId(row.id);      
+      const reactivationDate = new Date(dayjs(row.reactivationDate).format('YYYY/MM/DD'));
+      const action = row.action === 'CADUCAR' ? 'expire' : row.action === 'RENOVAR' ? 'renewal' : 'suspend';
+      const contractActions = row.action === 'CADUCAR' 
+        ? { active: false, enabled: false, deletedAt: new Date(dayjs().format('YYYY/MM/DD')) }
+          : row.action === 'RENOVAR' 
+            ? { isPremium: true, reactivationDate }
+            : { isPremium: false, reactivationDate };
+      const vehicleActions = row.action === 'CADUCAR' 
+        ? { enabled: false, deletedAt: new Date(dayjs().format('YYYY/MM/DD')) }
+          : row.action === 'RENOVAR' 
+            ? { isContractSuspended: false, isPremium: true } 
+            : { isContractSuspended: true, isPremium: false };
+      const updatedBy: UpdatedBy[] = [
+        {
+          updatedBy: new mongoose.Types.ObjectId(process.env.ADMIN_ID ?? ''),
+          updatedAt: new Date(dayjs().format('YYYY/MM/DD')),
+          action,
+        }
+      ];
       
-      console.log("Cantidad de registros de contratos a actualizar: ", contractBulkArr.length);
-      console.log("Cantidad de registros de vehículos a actualizar: ", vehiclesBulkArr.length);
-      console.log("Cantidad de registros totales a actualizar: ", contractBulkArr.length + vehiclesBulkArr.length);      
-    
-      console.log("Cantidad de registros con id inválido: ", errId.length);
+      vehiclesBulkArr.push({
+        updateOne: {
+          filter: { vin: row.vin },
+          //@ts-ignore
+          update: { $set: { ...vehicleActions } }
+        }
+      });
 
-      if (vehiclesBulkArr.length > 0 && contractBulkArr.length > 0) {
-        await contractsCollection?.bulkWrite(contractBulkArr, { ordered: false })
-        .then(result => {
-          console.log(`Se han encontrado ${result.matchedCount} contratos`);
-          console.log(`Se han modificado ${result.modifiedCount} contratos`);
-          console.log(`Se han eliminado ${result.deletedCount} contratos`);
-          console.log(`Se han insertado ${result.insertedCount} contratos`);
-          console.log(result);
-          // db.close()
-        })
-        .catch(err => (db.close(), createLog(err)));
+      contractBulkArr.push({
+        updateOne: {
+          filter: { _id },
+          update: { $set: { updatedBy, ...contractActions } }
+        }
+      });    
+    });
 
-        await vehiclesCollection.bulkWrite(vehiclesBulkArr, )
-        .then(result => {
-          console.log(`Se han encontrado ${result.matchedCount} vehículos`);
-          console.log(`Se han modificado ${result.modifiedCount} vehículos`);
-          console.log(`Se han eliminado ${result.deletedCount} vehículos`);
-          console.log(`Se han insertado ${result.insertedCount} vehículos`);
-          console.log(result);
-          db.close()
-        })
-        .catch(err => (db.close(), createLog(err)));
+    console.log("Cantidad de registros de contratos a actualizar: ", contractBulkArr.length);
+    console.log("Cantidad de registros de vehículos a actualizar: ", vehiclesBulkArr.length);
+    console.log("Cantidad de registros totales a actualizar: ", contractBulkArr.length + vehiclesBulkArr.length);      
+  
+    console.log("Cantidad de registros con id inválido: ", errId.length);
 
-      } else db.close();      
-    }).catch(err => (db.close(), createLog(err)));
+    errId.length > 0 && createLog(JSON.stringify(errId));
+
+    if (vehiclesBulkArr.length > 0 && contractBulkArr.length > 0) {
+      await db.createBulk(vehiclesCollection, vehiclesBulkArr, 'vehículos');
+      await db.createBulk(contractsCollection, contractBulkArr, 'contratos');
+    } else db.close();  
   } catch (error) {
     createLog(error) 
   }
