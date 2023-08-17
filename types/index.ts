8c05d0f1bce611b0c088aabbd850724396aebb45
Diego Garcia <dgarciavallejos@gmail.com>
2023-08-17 17:25:04 -0400

    update types

--- types/index.ts
+++ types/index.ts
@@ -1,26 +1,28 @@
-import { type ReturnModelType } from '@typegoose/typegoose'
-import { type BeAnObject } from '@typegoose/typegoose/lib/types'
-import { type Collection } from 'mongoose'
-
 export interface FileType {
   id: string
-  vin: string
-  action: 'CADUCAR' | 'RENOVAR' | 'SUSPENDER'
-  reactivationDate: string
+  name: string
 }
 
-export interface IMongoDB<T> {
-  createCollection:(collectionName: T) => 
-    Collection<Document> | ReturnModelType<any, BeAnObject> | undefined
-  // createCollectionTypegoose?: <T>(cls: new (...args: any[]) => T) => ReturnModelType<any, BeAnObject> | undefined
-  createBulk: <T>(
-    collection: ReturnModelType<any, BeAnObject> | undefined,
-    bulkArr: T[],
-    collectionAlias: string
-  ) => Promise<void>
-  validateChanges?: <T>(
-    collection: ReturnModelType<new (...args: unknown[]) => T, BeAnObject>,
-    query: Record<string, unknown>,
-  ) => Promise<void>
-  close: () => void
+export interface DriverMutate {
+  phone: string
+  first_name: string
+  last_name: string
+  email: string
+  properties: Properties
+  type: string
+}
+
+export interface Properties {
+  custom: string
+}
+
+export interface GroupMutate {
+  name: string
+  assets?: string[]
+  vehicles?: string[]
+}
+
+export interface LinkMutate {
+  primary_id: number
+  secondary_id: number
 }
